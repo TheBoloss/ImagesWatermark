42c6b64926c286ad1a41a21e2675b0986ceb2521
Eugène Villotte <eugene.villotte@gmail.com>
2022-04-09 11:09:38 +0200

    first commit

---
+++ ImagesWatermark.php
@@ -0,0 +1,48 @@
+<?php
+
+if (!isset($argv[1]) OR !isset($argv[2]))
+{
+    echo "Missing arguments";
+    exit;
+}
+
+$sourceDir = $argv[1];
+$destDir = $argv[2];
+
+echo "Source: $sourceDir\n";
+echo "Destination: $destDir\n\n";
+
+$sourceFiles = [];
+$sourceHandle = opendir($sourceDir);
+if ($sourceHandle)
+{
+    while (false !== ($sourceFile = readdir($sourceHandle)))
+    {
+        if ($sourceFile != "." && $sourceFile != "..")
+        {
+            $sourceFiles[] = $sourceFile;
+            $sourceFilePath = $sourceDir . "/" . $sourceFile;
+            $mt = mime_content_type($sourceFilePath);
+            
+            $destFilePath = $destDir . "/" . $sourceFile;
+            if ($mt == 'image/jpeg') $image = imagecreatefromjpeg($sourceFilePath);
+            elseif ($mt == 'image/png') $image = imagecreatefrompng($sourceFilePath);
+            else {
+                echo "❌ File is not an image:  $sourceFile\n";
+                continue;
+            }
+            imagettftext($image, 90, 0, 20, 103, 0x000000, "fonts/arial.ttf", $sourceFile);
+            imagettftext($image, 90, 0, 22, 105, 0xffff00, "fonts/arial.ttf", $sourceFile);
+            if ($mt == 'image/jpeg') imagejpeg($image, $destFilePath);
+            elseif ($mt == 'image/png') imagepng($image, $destFilePath);
+            echo "✅ Watermark added to:    $sourceFile\n";
+            // else
+            // {
+            //     echo "File $sourceFile is not an image\n";
+            // }
+        }
+    }
+}
+closedir($sourceHandle);
+
+// var_dump($sourceFiles);
---
+++ README.md
@@ -0,0 +1,19 @@
+# ImagesWatermark
+
+Add image title as watermark on each image of a directory.
+
+Works with **PNG** and **JPG** images.
+
+## Use
+
+```
+php ImagesWatermark.php SourceFolder DestFolder
+```
+
+## Result
+
+- ### Before
+    ![](example/before.jpg)
+
+- ### After
+    ![](example/after.jpg)
